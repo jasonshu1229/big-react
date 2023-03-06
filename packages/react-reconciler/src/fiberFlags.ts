58860b09bb7640bbdd79cfd8cdc9430087737050
jasonshu1229 <shuhao1229@163.com>
2023-03-06 12:01:32 +0800

    fix: 分别将NoFlags,以及那几个副作用的标识位向左移,因为NoFlags应该为0

--- packages/react-reconciler/src/fiberFlags.ts
+++ packages/react-reconciler/src/fiberFlags.ts
@@ -1,10 +1,10 @@
 export type Flags = number;
 
 // 每个 flag 占一个位
-export const NoFlags = 0b0000001; // 当前没有标记
-export const Placement = 0b0000010; // 插入
-export const Update = 0b0000100; // 更新属性
-export const ChildDeletion = 0b0001000; // 删除子节点
+export const NoFlags = 0b0000000; // 当前没有标记
+export const Placement = 0b0000001; // 插入
+export const Update = 0b0000010; // 更新属性
+export const ChildDeletion = 0b0000100; // 删除子节点
 
 // 如果当前节点 child flags 和 subtreeFlags 包括下面这三个副作用，则表示需要执行 mutation 阶段
 export const MutationMask = Placement | Update | ChildDeletion; // 变更标记
